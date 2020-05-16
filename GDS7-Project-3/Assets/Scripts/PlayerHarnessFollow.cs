272a6df7cda4c512ae0dc587a2d1bcc9a596263d
JacobMiki <JacobMiki@users.noreply.github.com>
2020-05-16 14:42:23 +0200

    Use Cinemachine for camera and add torch-like light

--- GDS7-Project-3/Assets/Scripts/PlayerHarnessFollow.cs
+++ GDS7-Project-3/Assets/Scripts/PlayerHarnessFollow.cs
@@ -5,11 +5,7 @@ using UnityEngine;
 public class PlayerHarnessFollow : MonoBehaviour
 {
     [SerializeField] private Transform _playerTransform;
-    [SerializeField] private Transform _camera;
     [SerializeField] private float _smoothing;
-    [SerializeField] private float _cameraHeight;
-    [SerializeField] private float _cameraDistance;
-    [SerializeField] private LayerMask _cameraOccluders;
 
     private Transform _transform;
 
@@ -22,19 +18,8 @@ public class PlayerHarnessFollow : MonoBehaviour
     {
         _transform.position = Vector3.Lerp(
             _transform.position,
-            new Vector3(_playerTransform.position.x , _playerTransform.position.y + _cameraHeight, _playerTransform.position.z),
+            _playerTransform.position,
             _smoothing * Time.fixedDeltaTime
         );
-
-        var newCameraPos = _transform.position + _transform.forward * -_cameraDistance;
-
-
-        if (Physics.Linecast(transform.position, newCameraPos, out var wallHit, _cameraOccluders))
-        {
-            newCameraPos = new Vector3(wallHit.point.x + wallHit.normal.x * 0.5f, _camera.position.y, wallHit.point.z + wallHit.normal.z * 0.5f);
-        }
-
-        _camera.position = Vector3.Lerp(_camera.position, newCameraPos, _smoothing * Time.fixedDeltaTime);
-
     }
 }
