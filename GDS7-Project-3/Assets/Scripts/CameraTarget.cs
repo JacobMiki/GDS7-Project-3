1f27caa742d20c0f296ecc2cde1c50f014887c19
JacobMiki <JacobMiki@users.noreply.github.com>
2020-08-01 14:39:16 +0200

    First climbing prototype

---
+++ GDS7-Project-3/Assets/Scripts/CameraTarget.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+
+public class CameraTarget : MonoBehaviour
+{
+    [SerializeField] private CinemachineTargetGroup _cinemachineTargetGroup;
+
+    public void AddPlayer()
+    {
+        _cinemachineTargetGroup.AddMember(GameObject.FindGameObjectWithTag("Player").transform, 1f, 0f);
+    }
+
+    public void RemovePlayer()
+    {
+        _cinemachineTargetGroup.RemoveMember(GameObject.FindGameObjectWithTag("Player").transform);
+    }
+}
---
+++ GDS7-Project-3/Assets/Scripts/ClimbingSpot.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using GDS7.Group1.Project3.Assets.Scripts;
+using GDS7.Group1.Project3.Assets.Scripts.State;
+using UnityEngine;
+
+public class ClimbingSpot : MonoBehaviour
+{
+    [SerializeField] private Transform _target;
+    [SerializeField] private float _targetTolerance;
+    [SerializeField] private float _moveSpeed;
+    [SerializeField] private float _maxDegreesToTargetForward;
+
+    private bool _isClimbing = false;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (_isClimbing || !other.CompareTag("Player"))
+        {
+            return;
+        }
+
+        if (!other.GetComponent<IGroundedState>().IsGrounded && Vector3.Angle(other.transform.forward, _target.forward) <= _maxDegreesToTargetForward)
+        {
+            _isClimbing = true;
+            other.GetComponent<CharacterInput>().InputsEnabled = false;
+            other.GetComponent<Rigidbody>().isKinematic = true;
+            StartCoroutine(MoveToTarget(other.transform));
+        }
+    }
+
+    private IEnumerator MoveToTarget(Transform other)
+    {
+        while(Vector3.Distance(other.position, _target.position) > _targetTolerance)
+        {
+            other.position = Vector3.Slerp(other.position, _target.position, Time.deltaTime * _moveSpeed);
+            yield return null;
+        }
+        other.GetComponent<CharacterInput>().InputsEnabled = true;
+        other.GetComponent<Rigidbody>().isKinematic = false;
+        _isClimbing = false;
+    }
+}
