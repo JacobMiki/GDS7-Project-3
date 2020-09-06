b459d9a30dcdfce19e2ca98441766fd960590de7
JacobMiki <JacobMiki@users.noreply.github.com>
2020-09-06 15:55:00 +0200

    Add Menu scene and script transition to gameplay

---
+++ GDS7-Project-3/Assets/Scripts/Menu.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using GDS7.Group1.Project3.Assets.Scripts.Scene;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class Menu : MonoBehaviour
+{
+    [SerializeField] private GameObject _blackScreen;
+    [SerializeField] private GameObject _background;
+    [SerializeField] private GameObject _menus;
+    [SerializeField] private GameObject _canSkipText;
+    [SerializeField] private Selectable _video;
+    [SerializeField] private VideoPlayer _introPlayer;
+    [SerializeField] private Level _startingLevel;
+
+    public void StartGame()
+    {
+        _blackScreen.SetActive(true);
+        Destroy(_background);
+        Destroy(_menus);
+        _video.gameObject.SetActive(true);
+        _video.Select();
+        _introPlayer.loopPointReached += _introPlayer_loopPointReached;
+        _introPlayer.targetCamera = Camera.main;
+        _introPlayer.Play();
+        StartCoroutine(WaitForLevelReady());
+        StartCoroutine(LevelLoader.LoadLevel(_startingLevel));
+    }
+
+    private void _introPlayer_loopPointReached(VideoPlayer source)
+    {
+        SwitchToGame();
+    }
+
+    public void SkipIntro()
+    {
+        if (LevelLoader.LevelReady)
+        {
+            _introPlayer.loopPointReached -= _introPlayer_loopPointReached;
+            SwitchToGame();
+        }
+    }
+
+    private IEnumerator WaitForLevelReady()
+    {
+        while (!LevelLoader.LevelReady)
+        {
+            yield return null;
+        }
+        _canSkipText.SetActive(true);
+    }
+
+    private void SwitchToGame()
+    {
+        SceneManager.activeSceneChanged += (oldS, newS) =>
+        {
+            SceneManager.UnloadSceneAsync("_Menu");
+        };
+        LevelLoader.CanActivateLevel = true;
+        Destroy(_introPlayer.gameObject, 0);
+    }
+}
--- GDS7-Project-3/Assets/Scripts/Scene/LevelLoader.cs
+++ GDS7-Project-3/Assets/Scripts/Scene/LevelLoader.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -8,36 +9,82 @@ namespace GDS7.Group1.Project3.Assets.Scripts.Scene
     public class LevelLoader : MonoBehaviour
     {
         [SerializeField] private Level _level;
-        [SerializeField] private GameObject _loadingScreen;
+        [SerializeField] private bool _loadOnStart;
+
+        public static bool CanActivateLevel = false;
+        public static bool LevelReady = false;
+
         void Start()
         {
-            _loadingScreen.SetActive(true);
-            StartCoroutine(StartAsync());
+            if (Application.isEditor && _loadOnStart)
+            {
+                StartCoroutine(StartAsync());
+            } else {
+                var scene = SceneManager.GetSceneByName("_Menu");
+                if (!scene.isLoaded)
+                {
+                    SceneManager.LoadSceneAsync("_Menu", LoadSceneMode.Additive);
+                }
+            }
         }
 
         public IEnumerator StartAsync()
         {
-            if (Application.isEditor)
-            {
-                yield return UnloadLevel(_level);
-            }
+            yield return UnloadLevel(_level);
             yield return LoadLevel(_level);
         }
 
-        public IEnumerator LoadLevel(Level level)
+        public static IEnumerator LoadLevel(Level level)
         {
+            LevelReady = false;
             var levelScene = SceneManager.CreateScene(level.Name);
 
+
+            var scenesToLoad = new List<AsyncOperation>();
             foreach (var levelPart in level.Parts)
             {
-                yield return SceneManager.LoadSceneAsync(levelPart.SceneName, LoadSceneMode.Additive);
+                var sceneLoadOperation = SceneManager.LoadSceneAsync(levelPart.SceneName, LoadSceneMode.Additive);
+                sceneLoadOperation.allowSceneActivation = false;
+                scenesToLoad.Add(sceneLoadOperation);
+            }
+
+            float progress = 0;
+            while (progress < 0.9f)
+            {
+                progress = 0;
+                for (int i = 0; i < scenesToLoad.Count; i++)
+                {
+                    progress += scenesToLoad[i].progress;
+                }
+                progress /= scenesToLoad.Count;
+                Debug.Log($"Level loading progess: {progress}");
+                yield return null;
+            }
+
+            LevelReady = true;
+
+            while (!CanActivateLevel)
+            {
+                yield return null;
+            }
+
+            for (int i = 0; i < scenesToLoad.Count; i++)
+            {
+                scenesToLoad[i].allowSceneActivation = true;
+            }
+
+            for (int i = 0; i < scenesToLoad.Count; i++)
+            {
+                while (!scenesToLoad[i].isDone)
+                {
+                    yield return null;
+                }
             }
 
             SceneManager.SetActiveScene(levelScene);
-            _loadingScreen.SetActive(false);
         }
 
-        public IEnumerator UnloadLevel(Level level)
+        public static IEnumerator UnloadLevel(Level level)
         {
             var scene = SceneManager.GetSceneByName(level.Name);
             if (scene.isLoaded)
