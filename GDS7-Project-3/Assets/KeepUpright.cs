d70a7de13458cfc23c837a5a0fec31d4aee826e7
JacobMiki <JacobMiki@users.noreply.github.com>
2020-09-13 14:31:02 +0200

    Fix torch shadow and attack eyes

---
+++ GDS7-Project-3/Assets/KeepUpright.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GDS7.Group1.Project3.Assets
+{
+    public class KeepUpright : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
---
+++ GDS7-Project-3/Assets/Scripts/KeepUpright.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GDS7.Group1.Project3.Assets.Scripts
+{
+    public class KeepUpright : MonoBehaviour
+    {
+        // Update is called once per frame
+        void Update()
+        {
+            transform.rotation = Quaternion.LookRotation(Vector3.forward, Vector3.up);
+        }
+    }
+}
