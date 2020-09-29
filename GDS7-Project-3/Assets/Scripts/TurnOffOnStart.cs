7ab51250d7d31381f8b8ed80f4e84b703037f2e7
JacobMiki <JacobMiki@users.noreply.github.com>
2020-09-30 00:07:28 +0200

    Smooth brasier light up

---
+++ GDS7-Project-3/Assets/Scripts/TurnOffOnStart.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GDS7.Group1.Project3.Assets.Scripts
+{
+    [RequireComponent(typeof(Light))]
+    public class TurnOffOnStart : MonoBehaviour
+    {
+        private Light _light;
+
+        void Start()
+        {
+            _light = GetComponent<Light>();
+            _light.intensity = 0.0f;
+        }
+    }
+}
